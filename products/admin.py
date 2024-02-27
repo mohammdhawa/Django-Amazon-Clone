8d19c61fc01aa48568a0589c4a2cd13bf7c29212
mohammdhawa <ragab5434@gmail.com>
2024-02-28 01:40:08 +0300

    add models to adming, add tabular inline

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Product, ProductImages, Brand, Review
 
 # Register your models here.
+
+admin.site.register(Brand)
+
+
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
+
+
+class ReviewAdmin(admin.TabularInline):
+    model = Review
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesAdmin, ReviewAdmin]
+
+
+admin.site.register(Product, ProductAdmin)
