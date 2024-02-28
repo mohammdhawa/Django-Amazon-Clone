53c57f23770360c61c6cd3310f8faa6d1327e8f6
mohammdhawa <ragab5434@gmail.com>
2024-02-28 15:13:26 +0300

    add seed_brand function to insert dummy data to brand

--- dummy_data.py
+++ dummy_data.py
@@ -1,9 +1,26 @@
 import django
 import os
-from faker import Faker
-import random
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
+from faker import Faker
+import random
+from products.models import Product, Brand, ProductImages, Review
+from django.contrib.auth.models import User
 
 
+
+
+def seed_brand(n):
+    fake = Faker()
+
+    images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg',
+              '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg']
+
+    for _ in range(n):
+        Brand.objects.create(
+            name=fake.name(),
+            image=f"brand/{images[random.randint(0, 9)]}"
+        )
+
+
+seed_brand(200)
