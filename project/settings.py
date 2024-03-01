414b9e55539d38a8f8760d93bddfa6541a0e44b2
mohammdhawa <ragab5434@gmail.com>
2024-03-01 14:57:52 +0300

    add default pagination for the api

--- project/settings.py
+++ project/settings.py
@@ -46,6 +46,12 @@ INSTALLED_APPS = [
     'products',
 ]
 
+# API Pagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
