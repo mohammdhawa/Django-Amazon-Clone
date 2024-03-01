c24eb31d592d0354a3826af77e0e582df101d019
mohammdhawa <ragab5434@gmail.com>
2024-03-01 16:17:54 +0300

    show average rate in product list & detail API

--- products/serializers.py
+++ products/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
-from .models import Product, Brand
+from .models import Product, Brand, Review
 
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
     reviews_count = serializers.SerializerMethodField()
+    avg_rate = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
@@ -13,11 +14,21 @@ class ProductListSerializer(serializers.ModelSerializer):
     def get_reviews_count(self, obj):
         reviews = obj.product_review.all().count()
         return reviews
+
+    def get_avg_rate(self, obj):
+        reviews = obj.product_review.all()
+        if reviews:
+            total_rates = sum(review.rate for review in reviews)
+            average = total_rates / len(reviews)
+            return round(average, 1)
+        else:
+            return 0.0
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
     reviews_count = serializers.SerializerMethodField()
+    avg_rate = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
@@ -26,6 +37,15 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     def get_reviews_count(self, obj):
         reviews = obj.product_review.all().count()
         return reviews
+
+    def get_avg_rate(self, obj):
+        reviews = obj.product_review.all()
+        if reviews:
+            total_rates = sum(review.rate for review in reviews)
+            average = total_rates / len(reviews)
+            return round(average, 1)
+        else:
+            return 0.0
 
 
 class BrandListSerializer(serializers.ModelSerializer):
