d25eb4d97eb171ec479b0915e1f77303261d638f
mohammdhawa <ragab5434@gmail.com>
2024-03-06 12:45:31 +0300

    add cart context processor

--- project/settings.py
+++ project/settings.py
@@ -90,6 +90,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'settings.settings_context_processor.get_settings',
+                'orders.order_context_processor.get_cart_data',
             ],
         },
     },
